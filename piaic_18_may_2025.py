85e2abc10e74ca1e0a8109e67dba976ca93c4867
Muhammad Wasif Khan <wasifp9@gmail.com>
2025-05-18 16:27:36 +0500

    PIAIC Code Uploaded to Repo From Starting

---
+++ piaic_18_may_2025.py
@@ -0,0 +1,35 @@
+# -----------------------------------------------------------------------------------------
+# 18 MAY 2025 SUNDAY
+
+# def greet(name:str,age:int)->str:
+#     return f"Hello ,{name} {age}"
+
+# funcCall=greet("Hamzah",22)
+
+# print(funcCall)
+
+# Dictionary in Python
+
+stuDetails={
+    "name":"Wasif",
+    "age":21,
+    "rollNo":123,
+    "address":"Karachi"
+}
+
+
+print(f''' 
+Name= {stuDetails['name']}
+Age= {stuDetails['age']}
+Roll No= {stuDetails['rollNo']}
+ ''')
+
+
+
+
+# for key,value in stuDetails.items():
+#     if value==123:
+#         print(key)
+
+# Sortinh the Array in python in ascending and decending order
+# myList=[9,1,8,3,5]
---
+++ practice.py
@@ -0,0 +1,207 @@
+# first_name="Wasif"
+# print(type(first_name)==str)
+
+# age=input('Enter Your Age: ')
+# print(age)
+
+# if type(age) == str:
+#     print("Invalid Input")
+# else:
+#     print(f"your age is (age)")
+
+# if age.isdigit():
+#     print(age)
+# else:
+#     print("Please Enter the input in digit")
+
+
+# if age.isdigit() == True:
+#     print(age)
+
+# else:
+#     print("Please Enter The Valid Age")
+
+
+# babarRuns=input("Enter the Runs")
+
+
+# if  babarRuns >50 and babarRuns.isdigit():
+#     print("Babar is King")
+# else:
+#     print("Babar is not King")
+
+
+# babarRuns=11
+# totalBalls=80
+
+# if babarRuns>50 and totalBalls> babarRuns:
+#     print('Test Match Innings')
+# elif babarRuns>50 and totalBalls<babarRuns:
+#     print("Babar is King")
+# else:
+#     print("Not a Good Player")
+
+
+# babar_runs=100
+# total_balls=20
+
+# if babar_runs>=50 and total_balls<babar_runs:
+#     print("king")
+# else:
+#     print("not a king") 
+
+# babar_runs=100
+# total_balls=120
+# totalSixes=2
+
+# if babar_runs >=50 and total_balls<babar_runs or totalSixes>= 5:
+#     print("Babar is King")
+# else:
+#     print("Babar is not a Good Player")
+
+# list2=["Wasif","Hassan","Mubeen",12,"Raza","Ahmed",35,99]
+# print(list2)
+# print(type(list2))
+
+# print(list2[1]=="Mubeen")
+# print(list2[6])
+# print(list2[6]==35)
+# print(len(list2))
+# print(list2[2],list2[3])
+# print(index("Hassan"))
+
+# print(dir(list2))
+
+# print(list2.index("Hassan"))
+# print(list2.index("Ahmed"))
+# list2.pop()
+# print(list2)
+# print(list2.remove("Mubeen"))
+# print(list2)
+# list2.append("Imran")
+# list2.pop(-3)
+# list2.insert(0,12)
+# list2.reverse();
+
+# print(list2)
+# print(dir(list2))
+
+# list2=["Wasif","Hassan","Mubeen",12,"Raza","Ahmed",35,99]
+
+# list2.insert(2,"Khan")
+# print(list2)
+# Insert and Remove Indexing will start from 1
+
+# list2.remove("Hassan")
+# print(list2[0],list2[1])
+
+# Functions in Python
+
+# def green():
+#     print("Hello From the function")
+
+# green()
+
+
+# def sumTwoNumbers(a,b):
+#     print(a+b)
+
+# sumTwoNumbers(2,2)
+
+# def sum(num1,num2):
+#     print("num1+num2")
+
+# sum(num1=3,num2=4)
+# sum(5,12)
+# t=input("Enter the Arithmetic Operator")
+
+
+# def calculate(a,b):
+#     t=input("Enter the Arithmetic Operator: ")
+#     if(t=="+"):
+#         print("Sum of the two numbers: ",a+b)
+#     elif(t=="-"):
+#         print("Difference of the two numbers: ",a-b)
+#     elif(t=="*"):
+#         print("Multiplication of the two numbers: ",a*b)
+#     else:
+#         print("Division of the two numbers: ",a/b)
+
+
+# calculate(15,10)
+
+# def calculator(num1,num2,operation):
+#     if operation=="+":
+#         print("The sum of two numbers:",num1+num2)
+#     elif operation == "-":
+#         print("The Difference of two numbers:",num1-num2)
+#     elif operation == "*":
+#         print("The Multiplication of two numbers:",num1*num2)
+#     elif operation == "/":
+#         print("The Division of two numbers:",num1/num2)
+#     else:
+#         print("Enter the valid operator")
+
+# # calculator(12,12,"@")
+# num1=int(input("Enter the first number: "))
+# num2=int(input("Enter the second number: "))
+# operation=input("Enter the operation: ")
+
+# def calculator(num1,num2,operation):
+#     if operation=="+":
+#         print("The sum of two numbers:",num1+num2)
+#     elif operation == "-":
+#         print("The Difference of two numbers:",num1-num2)
+#     elif operation == "*":
+#         print("The Multiplication of two numbers:",num1*num2)
+#     elif operation == "/":
+#         print("The Division of two numbers:",num1/num2)
+#     else:
+#         print("Enter the valid operator")
+
+# calculator(num1,num2,operation)
+
+# food1=input("Enter your Favourite Food 1: ")
+# food2=input("Enter your Favourite Food 2: ")
+# food3=input("Enter your Favourite Food 3: ")
+
+# def findTotalCalorie():
+#     beefCalorie=155
+#     riceCalorie=176
+#     wheatCalorie=221
+#     vegetableCalorie=300
+#     milkCalorie=300
+#     yogurtCalorie=900
+#     chickenCalorie=1200
+#     fishCalorie=677
+
+
+
+# def greet(age,name = "User"):
+#     print(f'Hello {name} you are {age} years old')
+
+# greet(23,"Wasif")
+
+# Return in Python
+
+# def catch(item):
+#     print("TAPPING")
+#     return f'Catched {item}'
+#     print("Done")
+
+
+# # Whenever Function Returns Anything then After the return keyword function Termintes
+# bag=catch("⚽")
+# print(bag)
+
+
+
+
+
+
+
+
+
+
+
+
